0bcbb8967e3e801ea9b499042c392a0442459e39
Gerard Wilkinson <gerard.wilkinson@me.com>
2014-10-12 11:38:45 +0100

    Adding Wax9Reader

---
+++ ExpressiveTouch/Wax9Reader.swift
@@ -0,0 +1,9 @@
+//
+//  Wax9Reader.swift
+//  ExpressiveTouch
+//
+//  Created by Gerard Wilkinson on 11/10/2014.
+//  Copyright (c) 2014 Newcastle University. All rights reserved.
+//
+
+import Foundation
